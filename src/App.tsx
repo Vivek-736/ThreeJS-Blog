aff9041b4e249f997b3b3499269fd54e6c658d20
Vivek <vivekousik8555@gmail.com>
2025-05-15 15:34:15 +0530

    done with the project setup with vite + react

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <main className="max-w-7xl mx-auto">
+        <Navbar />
+      </main>
+    </>
+  );
+}
+
+export default App;
